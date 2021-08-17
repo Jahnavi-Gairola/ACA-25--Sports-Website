7e0821d914c254f97e2526b09bbd05798ba4c838
Jahnavi-Gairola <anijanu1356@gmail.com>
2021-08-17 18:20:21 +0530

    P.V.Sindhu :-)

---
+++ scriptResults.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD_0f6Th2_mSG8uXPQZFKjxXakZcXBvvwY",
+    authDomain: "match-results-e92a8.firebaseapp.com",
+    databaseURL: "https://match-results-e92a8-default-rtdb.firebaseio.com",
+    projectId: "match-results-e92a8",
+    storageBucket: "match-results-e92a8.appspot.com",
+    messagingSenderId: "51288114801",
+    appId: "1:51288114801:web:2931c83840d5d1fe373dd0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
